6411c725aeb4d4795bdbe4121f4c46d881411340
ChangHangeol <hgchang1@naver.com>
2025-01-07 22:56:26 +0900

    [FEAT] Create Modal command 추가

---
+++ src/csvcreator.ts
@@ -0,0 +1,26 @@
+import { App, Modal, Plugin } from 'obsidian';
+
+export default class CsvCreateModal extends Modal {
+    constructor(app: App) {
+        super(app);
+    }
+
+    onOpen() {
+        let {contentEl} = this;
+        contentEl.createEl('h2', {text: 'Create CSV Table'});
+
+        let inputEl = contentEl.createEl('input');
+        inputEl.value = 'Hello World';
+
+        let buttonEl = contentEl.createEl('button', {text: 'Create'});
+        buttonEl.addEventListener('click', () => {
+            console.log(inputEl.value);
+            this.close();
+        });
+    }
+
+    onClose() {
+        let {contentEl} = this;
+        contentEl.empty();
+    }
+}
